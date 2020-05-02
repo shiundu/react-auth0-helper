4dfc37ee77ea03cabac1b364bb72e6c99a4150cb
Ben <benshiundu@gmail.com>
2020-05-02 16:42:42 +0200

    fix(Hook): was returning undefined

--- src/core/auth0Context.tsx
+++ src/core/auth0Context.tsx
@@ -1,15 +1,6 @@
 import * as React from 'react';
 import { WithAuth0Props } from './types';
 
-const defaultValues = {
-  isLoading: undefined,
-  isAuthenticated: undefined,
-  logout: undefined,
-  user: undefined,
-  accessToken: undefined,
-  handleRedirectCallback: () => null,
-};
-
-export const Auth0Context = React.createContext(defaultValues);
+export const Auth0Context = React.createContext<WithAuth0Props | undefined>(undefined);
 export const AuthProvider = Auth0Context.Provider;
 export const useAuth0 = (): WithAuth0Props => React.useContext(Auth0Context);
