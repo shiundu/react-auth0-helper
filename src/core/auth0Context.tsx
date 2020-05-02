f0ee7db62c3a52f6e74a7447ca803b79cc509f63
Ben <benshiundu@gmail.com>
2020-05-02 18:21:09 +0200

    fix(DefaultValues): returned default values

--- src/core/auth0Context.tsx
+++ src/core/auth0Context.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import { WithAuth0Props } from './types';
 
-export const Auth0Context = React.createContext<WithAuth0Props | undefined>(undefined);
+const defaultValues = {
+  isLoading: undefined,
+  isAuthenticated: undefined,
+  logout: undefined,
+  user: undefined,
+  accessToken: undefined,
+  handleRedirectCallback: () => null,
+};
+
+export const Auth0Context = React.createContext<WithAuth0Props>(defaultValues);
 export const AuthProvider = Auth0Context.Provider;
 export const useAuth0 = (): WithAuth0Props => React.useContext(Auth0Context);
